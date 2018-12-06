3f754dd92271174ae143fa3bd40b50fb583074d9
Bob Zhou <zhoubo392@gmail.com>
2018-12-06 15:19:50 +0800

    [update] -> for lili

--- bot.py
+++ bot.py
@@ -18,19 +18,27 @@ class AlexBot(object):
         self.bot.enable_puid()
         self.command = Command()
         print(self.bot.groups())
-        group_name = 'iOS group'
+        # group_name = 'iOS group'
+        group_name = 'notify_group'
         self.group = ensure_one(self.bot.groups().search(group_name))
         # self.group.update_group(True)
         self.checkin = CheckIn()
 
-        checkin_group_name = 'checkin notify'
-        self.checkin_group = ensure_one(self.bot.groups().search(checkin_group_name))
+        # checkin_group_name = 'checkin notify'
+        # try:
+        #     self.checkin_group = ensure_one(self.bot.groups().search(checkin_group_name))
+        # except ValueError:
+        #     self.checkin_group = None
 
-        # self.friend_keeplive = \
-        #     ensure_one(self.bot.friends().search('keep-alive-bot'))
+        self.friend_keeplive = \
+            ensure_one(self.bot.friends().search('keep-alive-bot'))
 
-    # def keep_alive(self):
-    #     self.friend_keeplive.send('i am alive')
+    def keep_alive(self):
+        self.friend_keeplive.send('i am alive')
+
+    def hour_notify(self):
+        import datetime
+        self.group.send(u'整点报时：%s:00' % datetime.datetime.now().strftime("%H"))
 
     def notify_iOS_checkin(self):
         print('检查 iOS 打卡信息！！！！！')
@@ -38,11 +46,11 @@ class AlexBot(object):
         if msg:
             self.group.send(msg)
 
-    def notify_checkgroup_checkin(self):
-        print('检查打卡组打卡信息！！！！！')
-        msg = self.checkin.check_all_user('2')
-        if msg:
-            self.checkin_group.send(msg)
+    # def notify_checkgroup_checkin(self):
+    #     print('检查打卡组打卡信息！！！！！')
+    #     msg = self.checkin.check_all_user('2')
+    #     if msg:
+    #         self.checkin_group.send(msg)
 
     def load_kr_data(self):
         kr = Kr()
@@ -57,7 +65,7 @@ class AlexBot(object):
         import datetime
         
         file_path = os.getcwd() + '/beauty/'
-        file_name = file_path + datetime.datetime.now().strftime("%Y-%m-%d-%H-%m-%s")
+        file_name = file_path + datetime.datetime.now().strftime("%Y-%m-%d-%H-%m-%s.png")
         if os.path.exists(file_path) is False:
             os.makedirs(file_path)
         f = open(file_name, 'ab')
@@ -66,11 +74,14 @@ class AlexBot(object):
         return file_name
 
     def schedule_of_weekdays(self):
-        for check_time in ['10:00', '10:15', '10:30', '19:00', '19:15', '19:30', '19:45', \
-                    '20:00', '20:30', '20:45', '21:00', '21:30']:
-            schedule.every().days.at(check_time).do(self.notify_iOS_checkin)
-            time.sleep(5)
-            schedule.every().days.at(check_time).do(self.notify_checkgroup_checkin)
+        # for check_time in ['10:00', '10:15', '10:30', '19:00', '19:15', '19:30', '19:45', \
+        #             '20:00', '20:30', '20:45', '21:00', '21:30']:
+        for check_time in ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', \
+            '17:00', '18:00', '19:00', '20:00', '21:00']:
+            schedule.every().days.at(check_time).do(self.hour_notify)
+            # schedule.every().days.at(check_time).do(self.notify_iOS_checkin)
+            # time.sleep(5)
+            # schedule.every().days.at(check_time).do(self.notify_checkgroup_checkin)
 
     def resolve_command(self, text, sender, allow_group=None):
         """解析当前的 message 中的 command
@@ -90,10 +101,10 @@ class AlexBot(object):
             else:
                 print('result type = %s' % type(result))
                 file_name = self.write_image2file(result)
-                if allow_group == 1:
-                    self.group.send_image(file_name)
-                else:
-                    self.checkin_group.send_image(file_name)
+                # if allow_group == 1:
+                self.group.send_image(file_name)
+                # else:
+                #     self.checkin_group.send_image(file_name)
                 result = None
             return result
 
@@ -105,9 +116,9 @@ if __name__ == '__main__':
         signal.signal(signal.SIGTERM, quit)
 
         alex_bot = AlexBot()
-        # schedule.every(2).to(3).hours.do(alex_bot.keep_alive)
+        schedule.every(1).to(2).hours.do(alex_bot.keep_alive)
         alex_bot.schedule_of_weekdays()
-        schedule.every().days.at('9:40').do(alex_bot.load_kr_data)
+        # schedule.every().days.at('9:40').do(alex_bot.load_kr_data)
 
         @alex_bot.bot.register(alex_bot.group, TEXT)
         def iOS_router(msg):
@@ -115,10 +126,10 @@ if __name__ == '__main__':
             print("puid = %s" % msg.member.puid)  #puid
             return alex_bot.resolve_command(msg.text, msg.member, '1')
 
-        @alex_bot.bot.register(alex_bot.checkin_group, TEXT)
-        def check_in_router(msg):
-            print("checkin group puid = %s" % msg.member.puid)#puid
-            return alex_bot.resolve_command(msg.text, msg.member, '2')
+        # @alex_bot.bot.register(alex_bot.checkin_group, TEXT)
+        # def check_in_router(msg):
+        #     print("checkin group puid = %s" % msg.member.puid)#puid
+        #     return alex_bot.resolve_command(msg.text, msg.member, '2')
 
         # embed()
 
